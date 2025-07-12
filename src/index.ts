55b4e6bbb376253a50d376eb1ca92e8bb5a4393e
PATRICK VAN KERCKHOVE <patrick.vankerckhove@student.hogent.be>
2025-07-13 01:17:26 +0200

    refactoring voor testing

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,23 @@
 // src/index.ts
-import Koa from 'koa';
-import bodyParser from 'koa-bodyparser';
-import { getLogger } from './core/logging';
-import installRest from './rest';
-import config from 'config';
-import koaCors from '@koa/cors';
-import { initializeData } from './data';
-import type { AagisAppContext, AagisAppState } from './types/koa';
+import createServer from './createServer';
 
-const CORS_ORIGINS = config.get<string[]>('cors.origins');
-const CORS_MAX_AGE = config.get<number>('cors.maxAge');
+async function main() {
+  try {
+    const server = await createServer();
+    await server.start();
 
-async function main(): Promise<void> {
-  const app = new Koa<AagisAppState, AagisAppContext>();
+    async function onClose() {
+      await server.stop(); 
+      process.exit(0);
+    }
 
-  app.use(
-    koaCors({
-      origin: (ctx) => {
-        if (CORS_ORIGINS.indexOf(ctx.request.header.origin!) !== -1) {
-          return ctx.request.header.origin!;
-        }
-        // Not a valid domain at this point, let's return the first valid as we should return a string
-        return CORS_ORIGINS[0] || '';
-      },
-      allowHeaders: ['Accept', 'Content-Type', 'Authorization'],
-      maxAge: CORS_MAX_AGE,
-    }),
-  );
-  
-  app.use(bodyParser());
-
-  await initializeData();
-  installRest(app);
-  
-  app.listen(9000, () => {
-    getLogger().info('🚀 Server listening on http://127.0.0.1:9000');
-  });
+    process.on('SIGTERM', onClose);
+    process.on('SIGQUIT', onClose);
+    process.on('SIGINT', onClose);
+  } catch (error) {
+    console.log('\n', error);
+    process.exit(-1);
+  }
 }
 
 main();
