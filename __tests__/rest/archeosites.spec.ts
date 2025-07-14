d250112f874f88fd7581fe65a6ccfa9ed208d948
PATRICK VAN KERCKHOVE <patrick.vankerckhove@student.hogent.be>
2025-07-14 23:24:47 +0200

    alle CRUD's in test

--- __tests__/rest/archeosites.spec.ts
+++ __tests__/rest/archeosites.spec.ts
@@ -2,7 +2,7 @@
 import supertest from 'supertest';
 import createServer from '../../src/createServer';
 import type { Server } from '../../src/createServer';
-import { AstronomischEvent, WendeType } from '@prisma/client';
+import { AstronomischEvent, Prisma, WendeType } from '@prisma/client';
 import { prisma } from '../../src/data';
 
 const data = {
@@ -12,18 +12,18 @@ const data = {
       naam: 'test naam 1',
       land: 'test land 1',
       beschrijving: 'test beschrijving 1',
-      breedtegraad: '51.2',
-      lengtegraad: '-1.8',
-      hoogte: '100.5',
+      breedtegraad: new Prisma.Decimal('51.2'),
+      lengtegraad: new Prisma.Decimal('-1.8'),
+      hoogte: new Prisma.Decimal('100.5'),
       foto: '/images/testimage1.jpg',
     },{
       id: 2,
       naam: 'test naam 2',
       land: 'test land 2',
       beschrijving: 'test beschrijving 2',
-      breedtegraad: '50.5',
-      lengtegraad: '0.1',
-      hoogte: '0',
+      breedtegraad: new Prisma.Decimal('50.5'),
+      lengtegraad: new Prisma.Decimal('0.1'),
+      hoogte: new Prisma.Decimal('0'),
       foto: '/images/testimage2.jpg',
     }],
   wendes: [{
@@ -32,21 +32,21 @@ const data = {
     wendeType: WendeType.ZOMERZONNEWENDE,
     astronomischEvent: AstronomischEvent.OPGANG,
     datumTijd: new Date(2023, 5, 21, 4, 52),
-    azimuthoek: 50.0,
+    azimuthoek: new Prisma.Decimal('50.0'),
   },{
     id: 2,
     siteId: 1,
     wendeType: WendeType.WINTERZONNEWENDE,
     astronomischEvent: AstronomischEvent.OPGANG,
     datumTijd: new Date(2023, 11, 22, 8, 9),
-    azimuthoek: 128.0,
+    azimuthoek: new Prisma.Decimal('128.0'),
   }, {
     id: 3,
     siteId: 2,
     wendeType: WendeType.WINTERZONNEWENDE,
     astronomischEvent: AstronomischEvent.OPGANG,
     datumTijd: new Date(2023, 11, 21, 8, 58),
-    azimuthoek: 134.0,
+    azimuthoek: new Prisma.Decimal('134.0'),
   }],
   markers: [{
     id:1,
@@ -54,16 +54,16 @@ const data = {
     wendeId: 1,
     naam: 'test marker 1',
     beschrijving: 'test marker beschrijving 1',
-    breedtegraad: 51.18,
-    lengtegraad: -1.82,
+    breedtegraad: new Prisma.Decimal('51.18'),
+    lengtegraad: new Prisma.Decimal('-1.82'),
   }, {
     id:2,
     siteId: 2,
     wendeId: 3,
     naam: 'test marker 2',
     beschrijving: 'test marker beschrijving 2',
-    breedtegraad: 53.7,
-    lengtegraad: -6.48,
+    breedtegraad: new Prisma.Decimal('53.7'),
+    lengtegraad: new Prisma.Decimal('-6.48'),
   }],
 };
 const dataToDelete ={
@@ -73,7 +73,6 @@ const dataToDelete ={
 };
 
 describe('Archeosites', () => {
-
   let server: Server;
   let request: supertest.Agent;
 
@@ -81,7 +80,6 @@ describe('Archeosites', () => {
     server = await createServer();
     request = supertest(server.getApp().callback());
   });
-
   afterAll(async() => {
     await server.stop();
   });
@@ -95,7 +93,6 @@ describe('Archeosites', () => {
       await prisma.wende.createMany({data: data.wendes});
       await prisma.orientatieMarker.createMany({data: data.markers});
     });
-
     afterAll(async() => {
       await prisma.archeologischeSite.deleteMany({
         where: { id: { in: dataToDelete.archeosites}},
@@ -139,6 +136,61 @@ describe('Archeosites', () => {
     });
   });
 
+  describe('GET /api/archeosites/:id', () => {
+    beforeAll(async() => {
+      await prisma.archeologischeSite.createMany({data: data.archeosites});
+      await prisma.wende.createMany({data: data.wendes});
+      await prisma.orientatieMarker.createMany({data: data.markers});
+    });
+    afterAll(async() => {
+      await prisma.archeologischeSite.deleteMany({
+        where: { id: { in: dataToDelete.archeosites}},
+      });
+      await prisma.wende.deleteMany({
+        where: { id: { in: dataToDelete.wendes}},
+      });
+      await prisma.orientatieMarker.deleteMany({
+        where: { id: { in: dataToDelete.markers}},
+      });
+    });
+    it('should 200 and return the requested archeosite', async()=>{
+      const response = await request.get(`${url}/1`);
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual({
+        id:1,
+        naam: 'test naam 1',
+        land: 'test land 1',
+        beschrijving: 'test beschrijving 1',
+        breedtegraad: '51.2',
+        lengtegraad: '-1.8',
+        hoogte: '100.5',
+        foto: '/images/testimage1.jpg',
+        orientatieMarkers: [{
+          id:1,
+          wendeId: 1,
+          naam: 'test marker 1',
+          beschrijving: 'test marker beschrijving 1',
+          breedtegraad: '51.18',
+          lengtegraad: '-1.82',
+        }],
+        wendes: [{
+          id:1,
+          wendeType: WendeType.ZOMERZONNEWENDE,
+          astronomischEvent: AstronomischEvent.OPGANG,
+          datumTijd: '2023-06-21T02:52:00.000Z',
+          azimuthoek: '50',
+        },{
+          id: 2,
+          wendeType: WendeType.WINTERZONNEWENDE,
+          astronomischEvent: AstronomischEvent.OPGANG,
+          datumTijd: '2023-12-22T07:09:00.000Z',
+          azimuthoek: '128',
+        }],
+      });
+    });
+  });
+
   describe('POST /api/archeosites', () => {
     const archeositesToDelete: number[] = [];
 
@@ -170,6 +222,68 @@ describe('Archeosites', () => {
       expect(response.body.foto).toBe('/images/testimage.jpg');
 
       archeositesToDelete.push(response.body.id);
+    });
+  });
+
+  describe('PUT /api/archeosites', () => {
+    beforeAll(async() => {
+      await prisma.archeologischeSite.createMany({data: data.archeosites});
+      await prisma.wende.createMany({data: data.wendes});
+      await prisma.orientatieMarker.createMany({data: data.markers});
+    });
+    afterAll(async() => {
+      await prisma.archeologischeSite.deleteMany({
+        where: { id: { in: dataToDelete.archeosites}},
+      });
+      await prisma.wende.deleteMany({
+        where: { id: { in: dataToDelete.wendes}},
+      });
+      await prisma.orientatieMarker.deleteMany({
+        where: { id: { in: dataToDelete.markers}},
+      });
+    });
+    it('should 200 and return the updated archeologicSite', async()=>{
+      const response = await request.put(`${url}/1`)
+        .send({
+          naam: 'updated naam',
+          land: 'updated land',
+          beschrijving: 'updated beschrijving',
+          breedtegraad: new Prisma.Decimal('53.2'),
+          lengtegraad: new Prisma.Decimal('-10.8'),
+        });
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body.id).toEqual(1);
+      expect(response.body.naam).toBe('updated naam');
+      expect(response.body.land).toBe('updated land');
+      expect(response.body.beschrijving).toBe('updated beschrijving');
+      expect(response.body.breedtegraad).toBe('53.2');
+      expect(response.body.lengtegraad).toBe('-10.8');
+    });
+  });
+
+  describe('DELETE /api/archeosites/:id', () => {
+    beforeAll(async() => {
+      await prisma.archeologischeSite.createMany({data: data.archeosites});
+      await prisma.wende.createMany({data: data.wendes});
+      await prisma.orientatieMarker.createMany({data: data.markers});
+    });
+    afterAll(async() => {
+      await prisma.archeologischeSite.deleteMany({
+        where: { id: { in: dataToDelete.archeosites}},
+      });
+      await prisma.wende.deleteMany({
+        where: { id: { in: dataToDelete.wendes}},
+      });
+      await prisma.orientatieMarker.deleteMany({
+        where: { id: { in: dataToDelete.markers}},
+      });
+    });
+    it('should 204 and return nothing', async ()=>{
+      const response = await request.delete(`${url}/1`);
+
+      expect(response.statusCode).toBe(204);
+      expect(response.body).toEqual({});
     });
   });
 });
